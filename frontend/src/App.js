163bc7129048dda02dc6a4e50cf9e132a50b946f
DavidPinilla1 <d.pinilla@geekshubs.com>
2019-06-18 10:07:13 +0200

    Âusers view removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import Counter from './components/Counter/Counter.jsx';
-// import Register from './components/User/Register/Register.jsx'
-// class App extends React.Component {
-
-//   render() {
-//     return (
-//         <Counter start={1} />
-//     )
-//   }
-
-// }
-// export default App;
-
-
-
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Register from './views/User/Register/Register.jsx';
@@ -21,17 +5,7 @@ import Login from './views/User/Login/Login.jsx';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Error from './components/Error/Error.jsx'
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = props =>{
-return <h2>
-Usuario número:{props.match.params.id}
-   <br/> 
-   Users
-   </h2>;
-
-}
-
+import Index from './views/Index/Index.jsx'
 
 function App() {
   return (
@@ -39,8 +13,6 @@ function App() {
         <Header/>
       <Switch>
         <Route path="/" exact component={Index} />
-        <Route path="/about/" exact component={About} />
-        <Route path="/users/:id/" exact component={Users} />
         <Route path="/register/"  component={Register} />
         <Route path="/login/"  component={  Login } />
         <Route component={Error} />
